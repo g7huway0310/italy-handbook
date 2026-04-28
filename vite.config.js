af0703950bd3cc2b7f67c38d7a9cf59217ae32bf
g7huway0310 <g7huway0310@gmail.com>
2026-04-28 22:19:03 +0800

    Add GitHub Pages deployment workflow and update package configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 加上這一行，/italy-handbook/ 必須與您的 GitHub Repo 名稱完全一致！
+  base: '/italy-handbook/', 
 })
